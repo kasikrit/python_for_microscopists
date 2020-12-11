cdbbdf75297bc7196ac964a5e5f1c39724508381
Kasikrit Damkliang <kasikrit@gmail.com>
2020-12-11 19:29:22 +0700

    Update 177_albumentations_aug.py

--- 177_albumentations_aug.py
+++ 177_albumentations_aug.py
@@ -10,17 +10,16 @@ from skimage import io, img_as_ubyte
 import random
 import os
 from scipy.ndimage import rotate
+from tqdm import tqdm
 
 import albumentations as A
-images_to_generate=2000
 
 
 
-
-images_path="membrane/256_patches/images/" #path to original images
-masks_path = "membrane/256_patches/masks/"
-img_augmented_path="membrane/augmented_train_256/aug_img/" # path to store aumented images
-msk_augmented_path="membrane/augmented_train_256/aug_mask/" # path to store aumented images
+images_path="GitHub/python_for_microscopists/177/images/" #path to original images
+masks_path = "GitHub/python_for_microscopists/177/masks/"
+img_augmented_path="GitHub/python_for_microscopists/177/aug_images/" # path to store aumented images
+msk_augmented_path="GitHub/python_for_microscopists/177/aug_masks/" # path to store aumented images
 images=[] # to store paths of images from folder
 masks=[]
 
@@ -30,8 +29,9 @@ for im in os.listdir(images_path):  # read image name from folder and append its
 for msk in os.listdir(masks_path):  # read image name from folder and append its path into "images" array     
     masks.append(os.path.join(masks_path,msk))
 
-
+SIZE = 256
 aug = A.Compose([
+    A.Resize(SIZE, SIZE),
     A.VerticalFlip(p=0.5),              
     A.RandomRotate90(p=0.5),
     A.HorizontalFlip(p=1),
@@ -43,14 +43,12 @@ aug = A.Compose([
 
 #random.seed(42)
 
-i=1   # variable to iterate till images_to_generate
-
-
-while i<=images_to_generate: 
+images_to_generate=2000
+for i in tqdm(list(range(images_to_generate))): 
     number = random.randint(0, len(images)-1)  #PIck a number to select an image & mask
     image = images[number]
     mask = masks[number]
-    print(image, mask)
+    #print(image, mask)
     #image=random.choice(images) #Randomly select an image name
     original_image = io.imread(image)
     original_mask = io.imread(mask)
@@ -64,5 +62,5 @@ while i<=images_to_generate:
     new_mask_path = "%s/augmented_mask_%s.png" %(msk_augmented_path, i)
     io.imsave(new_image_path, transformed_image)
     io.imsave(new_mask_path, transformed_mask)
-    i =i+1
+
     
